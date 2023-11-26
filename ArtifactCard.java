63628b193daac1ad672ae4daa65495ca01d52b48
Tollgaermis <termis20@ku.edu.tr>
2023-11-26 22:18:42 +0300

    class templates

---
+++ ArtifactCard.java
@@ -0,0 +1,24 @@
+public class ArtifactCard {
+    private String name;
+    private Integer ID;
+
+    public void obtainAbility() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+}
---
+++ ArtifactDeck.java
@@ -0,0 +1,19 @@
+public class ArtifactDeck {
+    private Integer cardNum;
+
+    public void getTopCard() {
+        
+    }
+
+    public void decreaseNumCards() {
+        
+    }
+
+    public Integer getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(Integer cardNum) {
+        this.cardNum = cardNum;
+    }
+}

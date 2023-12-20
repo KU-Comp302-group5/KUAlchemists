a2efecdc3bc9db51ddca20a87bb7c234d801bf10
yigit <yates20@ku.edu.tr>
2023-12-20 22:36:38 +0300

    AlchemyMarker and Theory updated

--- src/domain/AlchemyMarker.java
+++ src/domain/AlchemyMarker.java
@@ -4,4 +4,35 @@ public class AlchemyMarker {
     private Aspect redAspect;
     private Aspect blueAspect;
     private Aspect greenAspect;
+    
+	public AlchemyMarker(Aspect redAspect, Aspect blueAspect, Aspect greenAspect) {
+		this.redAspect = redAspect;
+		this.blueAspect = blueAspect;
+		this.greenAspect = greenAspect;
+	}
+
+	public Aspect getRedAspect() {
+		return redAspect;
+	}
+
+	public void setRedAspect(Aspect redAspect) {
+		this.redAspect = redAspect;
+	}
+
+	public Aspect getBlueAspect() {
+		return blueAspect;
+	}
+
+	public void setBlueAspect(Aspect blueAspect) {
+		this.blueAspect = blueAspect;
+	}
+
+	public Aspect getGreenAspect() {
+		return greenAspect;
+	}
+
+	public void setGreenAspect(Aspect greenAspect) {
+		this.greenAspect = greenAspect;
+	}
+	
 }
--- src/domain/Theory.java
+++ src/domain/Theory.java
@@ -2,4 +2,27 @@ package domain;
 
 public class Theory {
 
+	private Aspect aspect;
+	private Ingredient ingredient;
+	
+	public Theory(Aspect aspect, Ingredient ingredient) {
+		this.aspect = aspect;
+		this.ingredient = ingredient;
+	}
+
+	public Aspect getAspect() {
+		return aspect;
+	}
+
+	public void setAspect(Aspect aspect) {
+		this.aspect = aspect;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
 }

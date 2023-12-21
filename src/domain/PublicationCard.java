73d477ad96e2df901454cfddfa0d20fa1bba126f
yigit <yates20@ku.edu.tr>
2023-12-21 08:55:04 +0300

    theory moved to publication card

--- src/domain/PublicationCard.java
+++ src/domain/PublicationCard.java
@@ -1,9 +1,29 @@
 package domain;
 
 public class PublicationCard {
-    private int ingredient;
+	private Aspect aspect;
+	private Ingredient ingredient;
+	
+	public PublicationCard(Aspect aspect, Ingredient ingredient) {
+		this.aspect = aspect;
+		this.ingredient = ingredient;
+	}
 
-    public PublicationCard(){
+	public Aspect getAspect() {
+		return aspect;
+	}
+
+	public void setAspect(Aspect aspect) {
+		this.aspect = aspect;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
         
     }
 }
--- src/domain/Theory.java
+++ src/domain/Theory.java
@@ -1,28 +1,4 @@
 package domain;
 
 public class Theory {
-
-	private Aspect aspect;
-	private Ingredient ingredient;
-	
-	public Theory(Aspect aspect, Ingredient ingredient) {
-		this.aspect = aspect;
-		this.ingredient = ingredient;
-	}
-
-	public Aspect getAspect() {
-		return aspect;
-	}
-
-	public void setAspect(Aspect aspect) {
-		this.aspect = aspect;
-	}
-
-	public Ingredient getIngredient() {
-		return ingredient;
-	}
-
-	public void setIngredient(Ingredient ingredient) {
-		this.ingredient = ingredient;
-	}
 }

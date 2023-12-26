6effed298972df959cc5c5ffd6c0a62b5c85091b
Eray Çakar <ecakar20@ku.edu.tr>
2023-12-26 17:32:02 +0300

    sell potion use case domain classes implemented according to d1 diagrams

--- src/domain/PotionSale.java
+++ src/domain/PotionSale.java
@@ -1,9 +1,30 @@
 package domain;
 
 public class PotionSale{
-    private boolean prediction;
+    private int prediction;
+    private Potion potion;
 
-    public PotionSale() {
-        this.prediction = false;
+    public PotionSale(Ingredient ingr1, Ingredient ingr2, int prediction) {
+        this.prediction = prediction;
+        this.potion = new Potion(ingr1, ingr2);
     }
+
+	public Potion getPotion() {
+		return potion;
+	}
+
+	public void setPotion(Potion potion) {
+		this.potion = potion;
+	}
+
+	public int getPrediction() {
+		return prediction;
+	}
+
+	public void setPrediction(int prediction) {
+		this.prediction = prediction;
+	}
+	
+
+    
 }
--- src/domain/controllers/SellPotionHandler.java
+++ src/domain/controllers/SellPotionHandler.java
@@ -1,11 +1,41 @@
 package domain.controllers;
 
 import domain.Ingredient;
+import domain.Player;
+import domain.PotionSale;
 
 public class SellPotionHandler {
 	
-	public void sellPotion(Ingredient ingr1, Ingredient ingr2, boolean prediction) {
-		int yigit;
+	public void sellPotion(Ingredient ingr1, Ingredient ingr2, int prediction, Player p) {
+		PotionSale ps = new PotionSale(ingr1, ingr2, prediction);
+		updatePlayer(ps, p);
 	}
-
+	
+	public void updatePlayer(PotionSale ps, Player p) {
+		int quality = ps.getPotion().getQuality();
+		int prediction = ps.getPrediction();
+		
+    	if(prediction==1) { //positive prediction
+    		if(quality==1) { //positive potion
+    			p.increaseGold(3);
+    		}
+    		if(quality==-1) { //negative potion
+    			p.increaseReputation(-1);
+    		}
+    	}
+    	
+    	if(prediction==0) { //positive/neutral prediction
+    		if(quality==1 || quality==0) { //positive or neutral potion
+    			p.increaseGold(2);
+    		}
+    		if(quality==-1) { //negative potion
+    			p.increaseReputation(-1);
+    		}
+    	}
+    	
+    	if(prediction==-1) { //'may be negative' prediction
+    		p.increaseGold(1);
+    	}
+    	
+	}
 }

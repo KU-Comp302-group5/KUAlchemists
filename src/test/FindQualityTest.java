b8a4d7a8be609ffc00b0682d3460c6264545b9f4
Eray Çakar <ecakar20@ku.edu.tr>
2024-01-02 23:15:46 +0300

    test with ingredients with aspects of different size and different sign added

--- src/test/FindQualityTest.java
+++ src/test/FindQualityTest.java
@@ -18,22 +18,26 @@ public class FindQualityTest {
 	
 	PotionSale ps = new PotionSale(new Ingredient("", 0 , a, a, a), new Ingredient("", 0 , a, a, a), 0);
 	
-	@Test
+	@Test //test for null entries
 	public void expectedNullPointerException() {
 		assertThrows(NullPointerException.class, ()-> {
 		    ps.findQuality(null, null); 
 		});
 		Ingredient ingr1 = new Ingredient(null, 0 ,null, null, null);
-		Ingredient ingr2 = new Ingredient("", 0 ,a, b, c);
+		Ingredient ingr2 = new Ingredient("2", 0 , a, b, c);
+		Ingredient ingr3 = new Ingredient("3", 0 , null, b, c);
 		assertThrows(NullPointerException.class, ()-> {
 		    ps.findQuality(ingr1, ingr2); 
 		});
 		assertThrows(NullPointerException.class, ()-> {
 		    ps.findQuality(ingr1, ingr1); 
 		});
+		assertThrows(NullPointerException.class, ()-> {
+		    ps.findQuality(ingr2, ingr3); 
+		});
 	}
 	
-	@Test
+	@Test //test with ingredients with all aspects equal
 	public void testWithSameIngredient() {
 		Ingredient ingr1 = new Ingredient("1", 0 , a, a, a);
 		Ingredient ingr2 = new Ingredient("2", 0 , a, b, c);
@@ -42,7 +46,7 @@ public class FindQualityTest {
 		assertEquals(0, ps.findQuality(ingr2, ingr3));
 	}
 	
-	@Test
+	@Test //test with aspects of same size --> return 0
 	public void testWithSameSize() {
 		Ingredient ingr1 = new Ingredient("1", 0 , a, b, a);
 		Ingredient ingr2 = new Ingredient("2", 0 , c, d, c);
@@ -53,7 +57,31 @@ public class FindQualityTest {
 		assertEquals(0, ps.findQuality(ingr3, ingr4));
 	}
 	
-	//test with different size different sign --> return 0
+/*  Aspect a = new Aspect(true, true);
+	Aspect b = new Aspect(false, true);
+	Aspect c = new Aspect(true, false);
+	Aspect d = new Aspect(false, false);
+*/
+	
+	@Test //test with aspects of different size different sign --> return 0
+	public void testWithDifferentSign() {
+		Ingredient ingr1 = new Ingredient("1", 0 , a, b, a);
+		Ingredient ingr2 = new Ingredient("2", 0 , d, b, a);
+		assertEquals(0, ps.findQuality(ingr1, ingr2));
+		
+		Ingredient ingr3 = new Ingredient("3", 0 , a, b, c);
+		Ingredient ingr4 = new Ingredient("4", 0 , a, c, c);
+		assertEquals(0, ps.findQuality(ingr3, ingr4));
+		
+		Ingredient ingr5 = new Ingredient("5", 0 , a, b, c);
+		Ingredient ingr6 = new Ingredient("6", 0 , a, b, b);
+		assertEquals(0, ps.findQuality(ingr5, ingr6));
+		
+		Ingredient ingr7 = new Ingredient("7", 0 , a, b, c);
+		Ingredient ingr8 = new Ingredient("8", 0 , d, c, b);
+		assertEquals(0, ps.findQuality(ingr7, ingr8));
+	}
+	
 	//test with different size same sign --> 1 if sign == true, -1 if sign == false
 	//test with more than one aspect with different size same sign (which aspect determines quality?)
 

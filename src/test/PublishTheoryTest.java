4bac632bff4828816c1ee21f382b8847de69f713
yigit <yates20@ku.edu.tr>
2024-01-04 23:44:56 +0300

    testTheoryInstanceCreated

--- src/test/PublishTheoryTest.java
+++ src/test/PublishTheoryTest.java
@@ -13,7 +13,11 @@ import org.junit.jupiter.api.TestInstance;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import domain.AlchemyMarker;
+import domain.Aspect;
+import domain.Ingredient;
 import domain.PublicationTrack;
+import domain.Theory;
 
 /**
  * This class is for testing publishTheory method of PublicationTrack class
@@ -37,9 +41,31 @@ class PublishTheoryTest {
 		assertTrue(publicationTrack instanceof PublicationTrack, "getInstance() does not return PublicationTrack");
 	}
 
-	@Test 
-	final void testPublishTheory() {
-		System.out.println("Not yet implemented"); // TODO
+	@Test // This tests that the correct Theory instance is created and publishedTheories are updated.
+	final void testTheoryInstanceCreated() {
+		
+		Aspect as1 = new Aspect(true, true);
+		Aspect as2 = new Aspect(true, false);
+		Aspect as3 = new Aspect(false, false);
+		Aspect as4 = new Aspect(false, true);
+		
+		Ingredient mushroom = new Ingredient("mushroom", 6, as4, as2, as3);
+		
+		AlchemyMarker marker2 = new AlchemyMarker(as1, as1, as1, 2);
+		
+		int playerNo = 1;
+		
+		publicationTrack.publishTheory(mushroom, marker2, playerNo);
+		
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getIngredient(), mushroom);
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getMarker(), marker2);
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getPlayerNo(), playerNo);
 	}
+	
+	@Test //
+	final void test() {
+		
+	}
+	
 
 }

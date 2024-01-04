9f0d8f4b4c9d69c7bd84b02e30d46d2e7c18c944
yigit <yates20@ku.edu.tr>
2024-01-05 00:08:05 +0300

    testMultipleTheories2

--- src/test/PublishTheoryTest.java
+++ src/test/PublishTheoryTest.java
@@ -102,7 +102,7 @@ class PublishTheoryTest {
 		assertFalse(publicationTrack.getAvailableAlchemies().contains(marker6));
 	}
 	
-	@Test // This tests 
+	@Test // This tests multiple theory publication case.
 	final void testMultipleTheories() {
 		
 		Aspect as1 = new Aspect(true, true);
@@ -135,7 +135,43 @@ class PublishTheoryTest {
 		assertEquals(publicationTrack.getPublishedTheories().get(2).getIngredient(), feather);
 		assertEquals(publicationTrack.getPublishedTheories().get(2).getMarker(), marker3);
 		assertEquals(publicationTrack.getPublishedTheories().get(2).getPlayerNo(), playerNo);
-		
-
+	
 	}
+	
+	@Test // This tests multiple theory publication case.
+	final void testMultipleTheories2() {
+		
+		Aspect as1 = new Aspect(true, true);
+		Aspect as2 = new Aspect(true, false);
+		Aspect as3 = new Aspect(false, false);
+		Aspect as4 = new Aspect(false, true);
+		
+		Ingredient flower = new Ingredient("flower", 7, as4, as3, as1);
+		Ingredient root = new Ingredient("root", 8, as2, as3, as4);
+		
+		AlchemyMarker marker4 = new AlchemyMarker(as2, as2, as2, 4);
+		AlchemyMarker marker5 = new AlchemyMarker(as1, as4, as3, 5);
+		
+		int playerNo1 = 1;
+		int playerNo2 = 2;
+		
+		publicationTrack.publishTheory(flower, marker4, playerNo2);
+		publicationTrack.publishTheory(root, marker5, playerNo1);
+		
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getIngredient(), flower);
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getMarker(), marker4);
+		assertEquals(publicationTrack.getPublishedTheories().get(0).getPlayerNo(), playerNo2);
+		
+		assertEquals(publicationTrack.getPublishedTheories().get(1).getIngredient(), root);
+		assertEquals(publicationTrack.getPublishedTheories().get(1).getMarker(), marker5);
+		assertEquals(publicationTrack.getPublishedTheories().get(1).getPlayerNo(), playerNo1);
+		
+		assertFalse(publicationTrack.getAvailableIngredients().contains(flower));
+		assertFalse(publicationTrack.getAvailableIngredients().contains(root));
+		
+		assertFalse(publicationTrack.getAvailableAlchemies().contains(marker4));
+		assertFalse(publicationTrack.getAvailableAlchemies().contains(marker5));
+		
+	}
+	
 }
